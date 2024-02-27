10f319e7485f83dbacf0bb9d01ea642095949429
Nicolastolinii <nicolas_tolini@hotmail.com>
2024-02-27 16:50:52 -0300

    test

--- src/main/java/com/dblog/dblog/DblogApplication.java
+++ src/main/java/com/dblog/dblog/DblogApplication.java
@@ -5,6 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class DblogApplication {
+	public String PORT = System.getenv("PORT");
 
 	public static void main(String[] args) {
 		SpringApplication.run(DblogApplication.class, args);
